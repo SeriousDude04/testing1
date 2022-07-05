eabbecab6e1f26550ab5ffe0287bcb955751f5ad
noob04coder <78252850+noob04coder@users.noreply.github.com>
2022-07-06 01:14:32 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,8 +1,8 @@
 import pytextnow
 import time
 import os
+client = pytextnow.Client("u.tlive006",sid_cookie=os.environ['sid'],csrf_cookie=os.environ['csrf'])
 def sample_response():
-  client = pytextnow.Client("u.tlive006",sid_cookie=os.environ['sid'],csrf_cookie=os.environ['csrf'])
   new_messages = client.get_unread_messages()
   #print(new_messages)
   if not new_messages:
@@ -10,8 +10,11 @@ def sample_response():
     return "Not recieved otp yet , try again"
   else:
     for message in new_messages:
-      message.mark_as_read()
-#       messageData = "```" +  + "```"
       otpVar = message.content
       return otpVar
   return "error in getting otp try again "
+def markUnread():
+  new_messages = client.get_unread_messages()
+  for message in new_messages:
+    message.mark_as_read()
+    return
